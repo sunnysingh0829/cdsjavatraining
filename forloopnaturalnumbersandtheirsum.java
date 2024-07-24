f73a2f64b1044c59b14b0fc51a7ea941dbf0aa27
sunnysingh001 <118869550+sunnysingh001@users.noreply.github.com>
2024-07-24 15:48:24 +0530

    Natural numbers and their sum using for loop

--- forloopnaturalnumbersandtheirsum.java
+++ forloopnaturalnumbersandtheirsum.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class forloopdemo {
+public class forloopnaturalnumbersandtheirsum {
     public static void main(String[]args)
     {
         Scanner sc=new Scanner (System.in);
