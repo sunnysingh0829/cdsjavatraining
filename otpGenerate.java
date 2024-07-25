7d1876df68124ed70f90193fca40f39e5dc2c789
sunnysingh001 <118869550+sunnysingh001@users.noreply.github.com>
2024-07-25 22:22:43 +0530

    Generate Otp

---
+++ otpGenerate.java
@@ -0,0 +1,9 @@
+import java.util.Random;
+public class otpGenerate {
+    public static void main(String[] args) {
+        //to generate 6 digit otp using Random class
+        Random sc=new Random()
+        int myOtp=sc.nextInt(1000000);
+        System.out.println("Your OTP is:"+myOtp+"Don't Share to anyone\n");
+    }
+}
