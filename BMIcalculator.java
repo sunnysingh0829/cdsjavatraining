d0dc95ef6c363ee69b896f75601a4b667896fcd0
sunnysingh001 <118869550+sunnysingh001@users.noreply.github.com>
2024-07-26 02:07:38 +0530

    BMI Calculator

---
+++ BMIcalculator.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class BMIcalculator
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("Enter Weight in Kilograms(Kg):");
+        double wt= sc.nextDouble();
+
+        System.out.println("Enter Height in Meters(m):");
+        double ht= sc.nextDouble();
+
+        double bmi= wt/ht*ht;
+
+        System.out.println("Your BMI is :"+bmi);
+
+        if(bmi<18.5)
+        {
+            System.out.println("You are Underweight.");
+        }
+        else if (bmi<24.9)
+        {
+            System.out.println("You Have normal Weight.");
+        }
+        else if (bmi<29.9)
+        {
+            System.out.println("You are Overweight.");
+        }
+        else
+        {
+            System.out.println("You are Obese.");
+        }
+    }
+}
